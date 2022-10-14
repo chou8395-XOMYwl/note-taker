ddf7a156207b576969ecfbdfdf1a43c4c39fc6b2
chou8395-XOMYwl <chou8395@gmail.com>
2022-10-14 11:06:56 -0400

    updated server

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3002;
 const express = require('express');
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
